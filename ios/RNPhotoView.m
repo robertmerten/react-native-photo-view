d244cd39dcf02272d9c2a13b8d05e81379aee306
Robert Merten [AYM] <robert.merten@aboutyou.de>
2017-08-29 13:33:02 +0200

    Set white background color

--- ios/RNPhotoView.m
+++ ios/RNPhotoView.m
@@ -367,7 +367,7 @@
     _maxZoomScale = 3.0;
     
     // Setup
-    self.backgroundColor = [UIColor blackColor];
+    self.backgroundColor = [UIColor whiteColor];
     self.delegate = self;
     self.decelerationRate = UIScrollViewDecelerationRateFast;
     self.showsVerticalScrollIndicator = YES;
@@ -377,12 +377,12 @@
     _tapView = [[MWTapDetectingView alloc] initWithFrame:self.bounds];
     _tapView.tapDelegate = self;
     _tapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    _tapView.backgroundColor = [UIColor blackColor];
+    _tapView.backgroundColor = [UIColor whiteColor];
     [self addSubview:_tapView];
     
     // Image view
     _photoImageView = [[MWTapDetectingImageView alloc] initWithFrame:self.bounds];
-    _photoImageView.backgroundColor = [UIColor blackColor];
+    _photoImageView.backgroundColor = [UIColor whiteColor];
     _photoImageView.contentMode = UIViewContentModeCenter;
     _photoImageView.tapDelegate = self;
     [self addSubview:_photoImageView];
